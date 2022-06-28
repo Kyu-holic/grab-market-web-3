547f2bc0dd6192b2979c6af736c4945b82f79175
unknown <khhan1990@hanmail.net>
2022-06-29 00:09:59 +0900

    antd 설치 후 상품 업로드 버튼까지 완성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import "antd/dist/antd.css";
 import "./App.css";
 import MainPageComponent from "./MainPage/index";
 import ProductPageComponent from "./ProductPage";
 import UploadPageComponent from "./UploadPage";
-import { Switch, Route } from "react-router-dom";
+import { Link, Switch, Route } from "react-router-dom";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
   return (
     <div>
       <div id="header">
         <div id="header-area">
-          <img src="/images/icons/logo.png" alt="" />
+          <Link to={"/"}>
+            <img src="/images/icons/logo.png" alt="" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function(){}}
+            icon={<DownloadOutlined />}
+          >상품 업로드</Button>
         </div>
       </div>
       <div id="body">
